275042522bda766e2d10b524c7ebd5d6461d53a5
Moznokasha <168994795+Moznokasha@users.noreply.github.com>
2025-09-09 04:13:59 +0300

    First commit - upload project

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,45 +1,12 @@
 package com.example.composeapp
 
-import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.snapping.SnapPosition
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.composeapp.ui.theme.ComposeAppTheme
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.focus.focusModifier
+import com.example.composeapp.screens.repoDetails.RepoDetailsContent
+
+import com.example.composeapp.theme.ComposeAppTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -48,11 +15,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                RepoDetailsContent()
+//                RepoScreen()
+
 //                    LazyColumn {
 //                        items(dummyData()){(title, description) ->
 //                            MyItem(title = title, description = description)
@@ -63,101 +28,10 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = "Details",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 24.sp
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = {  }) {
-                        Icon(Icons.Default.ArrowBack, contentDescription = "back")
-                    }
-                }
-            )
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .background(Color(0xFFE0E0E0)),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(R.drawable.google),
-                contentDescription = null,
-                modifier = Modifier.size(150.dp)
-            )
-            Text(
-                text = "Language",
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp
-            )
-            Row( modifier = Modifier
-                .padding(30.dp)
-                .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ){
-                    Text(
-                        text = "1525",
-                        fontSize = 15.sp,
-                        modifier = Modifier
-                            .padding(end = 5.dp)
-                    )
-                    Image(
-                        painter = painterResource(R.drawable.star),
-                        contentDescription = null
-                    )
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "Python",
-                        modifier = Modifier.padding(end = 5.dp))
-                    Image(painter = painterResource(R.drawable.blue),
-                        contentDescription = null)
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "347",
-                        fontSize = 15.sp,
-                        modifier = Modifier.padding(end = 5.dp))
-                    Image(painter = painterResource(R.drawable.fork),
-                        contentDescription = null)
-                }
-
-            }
-            Text(text = "Shared repository for open-sourced projects from the Google AI Language team. ",
-                modifier = Modifier.padding(30.dp),
-                fontSize = 20.sp)
-            Spacer(modifier = Modifier.height(300.dp))
-
-            Button(
-                onClick = {},
-                shape = RoundedCornerShape(5.dp),
-                modifier = Modifier.fillMaxWidth(0.7f),
 
 
-            ) {
-                Text(text = "Show Issues",
-                    fontSize = 25.sp)
-            }
-        }
-    }
-}
+
+
 
 
 //fun circularButtonModifier(color: Color) = Modifier
@@ -182,13 +56,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 
 
-@Preview(
-    showBackground = true ,
-    showSystemUi = true,
-    device = Devices.PIXEL_7)
-@Composable
-fun GreetingPreview() {
-    ComposeAppTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(
+//    showBackground = true ,
+//    showSystemUi = true,
+//    device = Devices.PIXEL_7)
---
+++ app/src/main/java/com/example/composeapp/theme/Type.kt
@@ -0,0 +1,75 @@
+package com.example.composeapp.theme
+
+import com.example.composeapp.R
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+//val Typography = Typography(
+//    bodyLarge = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+//    )
+
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_light, weight = FontWeight.Light),
+    Font(R.font.poppins_medium, weight = FontWeight.Medium),
+    Font(R.font.poppins_semibold, weight = FontWeight.SemiBold),
+)
+
+val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+    ),
+)
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
